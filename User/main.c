dcd1cf57bf85628cf521c63b1b211327c45cb00b
kitechin <kitechin@qq.com>
2023-03-25 22:54:58 +0800

    update

--- User/main.c
+++ User/main.c
@@ -3,27 +3,86 @@
 #include "ld303_bsp.h"
 #include "stm32f4xx.h"
 #include "sys.h"
+#include "timer.h"
 #include <stdio.h>
+#include <string.h>
+
+#define X_BASE 130
+#define Y_BASE 220
+#define X_LENGTH 150
+#define Y_LENGTH 150
+#define INTENS_MAX 15000
+#define DISTANCE_MAX 150
+
+void GUI_Init(void);
+void GUI_Axes_Display(void);
+void GUI_Digital_Display(int distance, int intensity);
+void GUI_Axes_Point(int distance, int intensity);
 
 int main(void)
 {
-    char distance[10] = { 0 };
-    char intensity[10] = { 0 };
-
-    delay_init(168); // 初始化延时函数
-    LCD_Init(); // LCD初始化
+    delay_init(168);
+    LCD_Init();
     LD303_Init();
-    Draw_Font24B(70, 20, BLACK, (u8*)"LD303 测距分析");
-    Draw_Font16B(90, 70, BLACK, (u8*)"测量距离:");
-    Draw_Font16B(90, 100, BLACK, (u8*)"信号强度:");
+    TIM6_Init();
+    GUI_Init();
 
     while (1) {
-		Draw_Font16B(170, 70, BLUE, (u8*)"              ");
-		Draw_Font16B(170, 100, RED, (u8*)"              ");
-		sprintf(distance, "%d", LD303_Dis_Get());
-		sprintf(intensity, "%d", LD303_Intensity_Get());
-		Draw_Font16B(170, 70, BLUE, (u8*)distance);
-		Draw_Font16B(170, 100, RED, (u8*)intensity);
-		delay_ms(50);	
+        if (Timer_ms_Get() >= 100) {
+            int distance = LD303_Dis_Get();
+            int intensity = LD303_Intensity_Get();
+
+            GUI_Digital_Display(distance,intensity);
+            GUI_Axes_Point(distance,intensity);
+            Timer_ms_Reset();
+        }
     }
+}
+
+void GUI_Axes_Point(int distance, int intensity)
+{
+    if (intensity == 0 || \
+        distance == 0 || \
+        intensity > INTENS_MAX || \
+        distance > DISTANCE_MAX) {
+        return;
+    }
+
+    int x = (int)(distance + 0.5);
+    int y = (int)(intensity / 100 + 0.5);
+
+    LCD_Fast_DrawPoint(X_BASE + x, Y_BASE - y, RED);
+}
+
+void GUI_Init(void)
+{
+    GUI_Axes_Display();
+    Draw_Font24B(80, 20, BLACK, (u8*)"LD303 测距分析");
+    Draw_Font16B(20, 100, BLACK, (u8*)"距离:");
+    Draw_Font16B(20, 150, BLACK, (u8*)"强度:");
+}
+void GUI_Digital_Display(int distance, int intensity)
+{
+    char c_distance[10] = { 0 };
+    char c_intensity[10] = { 0 };
+    char dis_unit[] = { 'c', 'm' };
+    char intens_unit[] = { 'k' };
+
+    sprintf(c_distance, "%d", distance);
+    sprintf(c_intensity, "%d", intensity);
+    strcat(c_distance, dis_unit);
+    strcat(c_intensity, intens_unit);
+
+    Draw_Font16B(60, 100, BLUE, (u8*)"        "); 
+    Draw_Font16B(60, 150, RED, (u8*)"        ");
+    Draw_Font16B(60, 100, BLUE, (u8*)c_distance);
+    Draw_Font16B(60, 150, RED, (u8*)c_intensity);
+}
+
+void GUI_Axes_Display(void)
+{
+    LCD_DrawLine(X_BASE, Y_BASE, X_BASE, Y_BASE - Y_LENGTH, BLACK);
+    LCD_DrawLine(X_BASE, Y_BASE, X_BASE + X_LENGTH, Y_BASE, BLACK);
+    Draw_Font16B(X_BASE + X_LENGTH, Y_BASE, BLUE, (u8*)"S");
+    Draw_Font16B(X_BASE - 10, Y_BASE - Y_LENGTH, BLUE, (u8*)"I");
 }
